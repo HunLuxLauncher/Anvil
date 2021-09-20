8fc3d82b808baf3271920c1590a1e07d11a419f2
Dávid Czompó <czompi@czompi.hu>
2021-09-20 14:36:19 +0200

    Save @ 2021-09-20 14:35

---
+++ Anvil/AnvilDatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+internal class AnvilDatabaseContext : DbContext
+{
+    public AnvilDatabaseContext([NotNull] DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        //use this to configure the contex
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //use this to configure the model
+    }
+
+    public DbSet<ForgeVersions> ForgeVersions { get; set; }
+    public DbSet<Mods> Mods { get; set; }
+    public DbSet<ModVersions> ModVersions { get; set; }
+    public DbSet<ModDependencies> ModDependencies { get; set; }
+}
--- Anvil/Program.cs
+++ Anvil/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,9 @@ builder.Services.AddSwaggerGen(c =>
     c.SwaggerDoc("v1", new() { Title = "Anvil", Version = "v1" });
 });
 
+builder.Services.AddDbContext<AnvilDatabaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AnvilDatabaseContext"))
+);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
